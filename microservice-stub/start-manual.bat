ad718a18a194c3565c9cc0d9a9c0f0c887675307
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-24 10:45:19 +0900

    Delete microservice-stub directory

--- microservice-stub/start-manual.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM ===== 개인계좌 수동정산 모드 실행 =====
-set API_KEY=CHANGE_ME_RANDOM
-set HMAC_SECRET=CHANGE_ME_RANDOM
-set MANUAL_BANK=토스뱅크
-set MANUAL_ACCOUNT=100009904574
-node server.js
-pause
